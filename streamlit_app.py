7cf49d4a8b203668d6af123c6956a0cc686922af
mykola-avram <62845601+mykola-avram@users.noreply.github.com>
2022-05-25 09:15:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,17 @@ import streamlit as st
 import pandas as pd
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+
+#df = pd.DataFrame({
+#  'first column': [1, 2, 3, 4],
+#  'second column': [10, 20, 30, 40]
+#})
+#df
+
 
