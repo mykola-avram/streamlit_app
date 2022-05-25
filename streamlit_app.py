9df7d0dd63024f4f5bccb518f1f5cb6b29a15e00
mykola-avram <62845601+mykola-avram@users.noreply.github.com>
2022-05-25 14:53:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,11 @@ fruit_to_show = my_fruit_list.loc[fruitsSelect]
 print('Print:', fruit_to_show)
 st.dataframe(fruitsSelect)
 
-
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+st.text("Hello from Snowflake:")
+st.text(my_data_row)
 
 
