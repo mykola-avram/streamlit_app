5efe85d0da8b9046429aff1df07a7307a9fbbc64
mykola-avram <62845601+mykola-avram@users.noreply.github.com>
2022-05-25 10:09:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,13 @@ st.text('Hard-Boiled Free-Range Egg')
 
 pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-my_fruit_list.set_index("Avocado")
+my_fruit_list.set_index("Fruit")
 
 # Let's put a pick list here so they can pick the fruit they want to include
 fruitsSelect = st.multiselect(
     "Pick some fruits:",
     #options=list(my_fruit_list.index),
-    options=list(my_fruit_list['Fruit']),
+    options=list(my_fruit_list['Avocado']),
     default=["Avocado","Strawberries"])
 
 
