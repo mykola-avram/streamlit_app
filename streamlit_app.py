6c395100b9f4e4c30b9e9b66dbe3fdc9410bbec5
mykola-avram <62845601+mykola-avram@users.noreply.github.com>
2022-05-25 15:12:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,6 +67,6 @@ st.text(my_data_row)
 my_cur.execute("select * from fruit_load_list")
 
 my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list con")
-streamlit.dataframe(my_data_rows)
+st.header("The fruit load list con")
+st.dataframe(my_data_rows)
 
